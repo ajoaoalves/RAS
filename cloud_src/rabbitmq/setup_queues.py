c4e30027bddf60066f5b041b6fb4379904a05783
Gustominox <agstcmps.111@gmail.com>
2025-01-09 19:58:43 +0000

    remove comments

--- cloud_src/rabbitmq/setup_queues.py
+++ cloud_src/rabbitmq/setup_queues.py
@@ -12,25 +12,6 @@ channel.exchange_declare(
         durable=True,
     )
 
-# tools_and_routing_keys = [
-#     ("border_tool", "border"),
-#     ("crop_tool", "crop"),
-#     ("rotation_tool", "rotation"),
-#     ("brightness_tool", "brightness"),
-#     ("binarization_tool", "binarization"),
-#     ("resize_tool", "resize"),
-#     ("count_people_tool", "count_people"),
-#     ("object_detection_tool", "object_detection"),
-#     ("background_removal_tool", "background_removal"),
-#     ("watermark-requests", "requests.watermark"),
-#     ("results", "results"),
-# ]
-
-
-# for queue, routing_key in tools_and_routing_keys:
-#     channel.queue_declare(queue=queue, durable=True)  # Declare the queue
-#     channel.queue_bind(exchange='picturas.tools', queue=queue, routing_key=routing_key)  # Bind the queue to the exchange
-
 for queue in QUEUES:
     channel.queue_declare(queue=queue["name"], durable=queue["durable"])
     channel.queue_bind(exchange=PICTURAS_EXCHANGE, queue=queue["name"], routing_key=queue["routing_key"])
