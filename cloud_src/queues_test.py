0d0524613ea7f7fd0e1326eccbf9e9c357764ea2
Gustominox <agstcmps.111@gmail.com>
2025-01-09 19:58:20 +0000

    Use rabbit config in tests

--- cloud_src/queues_test.py
+++ cloud_src/queues_test.py
@@ -1,29 +1,16 @@
 import pika
-
-# RabbitMQ connection parameters
-rabbitmq_host = 'localhost'  # Update this to your RabbitMQ container's hostname or IP if not localhost
-queues = [
-    "border_tool",
-    "crop_tool",
-    "rotation_tool",
-    "brightness_tool",
-    "binarization_tool",
-    "resize_tool",
-    "count_people_tool",
-    "object_detection_tool",
-    "background_removal_tool",
-]
+from rabbitmq.rabbit_config import *
 
 # Connect to RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
 channel = connection.channel()
 
 # Publish test messages to each queue
-for queue in queues:
-    message = f"Test message for {queue}"
+for queue in QUEUES:
+    message = f"Test message for {queue['name']}"
     channel.basic_publish(
-        exchange='',  # Default exchange
-        routing_key=queue,  # Queue name as routing key
+        exchange=PICTURAS_EXCHANGE,  # Default exchange
+        routing_key=queue['routing_key'],  # Queue name as routing key
         body=message
     )
     print(f" [x] Sent '{message}' to queue '{queue}'")
--- cloud_src/rabbitmq/setup_queues.py
+++ cloud_src/rabbitmq/setup_queues.py
@@ -1,37 +1,39 @@
 import pika
 from pika.exchange_type import ExchangeType
+from rabbit_config import *
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(HOST))
 channel = connection.channel()
 
 
 channel.exchange_declare(
-        exchange="picturas.tools",
+        exchange=PICTURAS_EXCHANGE,
         exchange_type=ExchangeType.direct,
         durable=True,
     )
 
-# List of queues with their routing keys
-tools_and_routing_keys = [
-    ("border_tool", "border"),
-    ("crop_tool", "crop"),
-    ("rotation_tool", "rotation"),
-    ("brightness_tool", "brightness"),
-    ("binarization_tool", "binarization"),
-    ("resize_tool", "resize"),
-    ("count_people_tool", "count_people"),
-    ("object_detection_tool", "object_detection"),
-    ("background_removal_tool", "background_removal"),
-    ("watermark-requests", "requests.watermark"),
-    ("results", "results"),
-]
+# tools_and_routing_keys = [
+#     ("border_tool", "border"),
+#     ("crop_tool", "crop"),
+#     ("rotation_tool", "rotation"),
+#     ("brightness_tool", "brightness"),
+#     ("binarization_tool", "binarization"),
+#     ("resize_tool", "resize"),
+#     ("count_people_tool", "count_people"),
+#     ("object_detection_tool", "object_detection"),
+#     ("background_removal_tool", "background_removal"),
+#     ("watermark-requests", "requests.watermark"),
+#     ("results", "results"),
+# ]
 
 
-# Declare and bind each queue
-for queue, routing_key in tools_and_routing_keys:
-    channel.queue_declare(queue=queue, durable=True)  # Declare the queue
-    channel.queue_bind(exchange='picturas.tools', queue=queue, routing_key=routing_key)  # Bind the queue to the exchange
+# for queue, routing_key in tools_and_routing_keys:
+#     channel.queue_declare(queue=queue, durable=True)  # Declare the queue
+#     channel.queue_bind(exchange='picturas.tools', queue=queue, routing_key=routing_key)  # Bind the queue to the exchange
 
+for queue in QUEUES:
+    channel.queue_declare(queue=queue["name"], durable=queue["durable"])
+    channel.queue_bind(exchange=PICTURAS_EXCHANGE, queue=queue["name"], routing_key=queue["routing_key"])
 
 print("Queues for all tools have been set up successfully.")
 
