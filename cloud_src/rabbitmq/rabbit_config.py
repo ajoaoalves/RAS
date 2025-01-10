378bfca0992184bbc32cdd53ddd48786df87fa95
Gustominox <agstcmps.111@gmail.com>
2025-01-10 11:40:48 +0000

    rename queues in rabbit config

--- cloud_src/rabbitmq/rabbit_config.py
+++ cloud_src/rabbitmq/rabbit_config.py
@@ -8,15 +8,15 @@ EXCHANGE_DURABLE = True
 
 # Queues and Routing keys
 QUEUES = [
-    {"name": "border_tool", "routing_key": "border", "durable": True},
-    {"name": "crop_tool", "routing_key": "crop", "durable": True},
-    {"name": "rotation_tool", "routing_key": "rotation", "durable": True},
-    {"name": "brightness_tool", "routing_key": "brightness", "durable": True},
-    {"name": "binarization_tool", "routing_key": "binarization", "durable": True},
-    {"name": "resize_tool", "routing_key": "resize", "durable": True},
-    {"name": "count_people_tool", "routing_key": "count_people", "durable": True},
-    {"name": "object_detection_tool", "routing_key": "object_detection", "durable": True},
-    {"name": "background_removal_tool", "routing_key": "background_removal", "durable": True},
-    {"name": "watermark-requests", "routing_key": "requests.watermark", "durable": True},
-    {"name": "results", "routing_key": "results", "durable": True},
+    {"name": "border-requests"              , "routing_key": "border"               , "durable": True},
+    {"name": "crop-requests"                , "routing_key": "crop"                 , "durable": True},
+    {"name": "rotation-requests"            , "routing_key": "rotation"             , "durable": True},
+    {"name": "brightness-requests"          , "routing_key": "brightness"           , "durable": True},
+    {"name": "binarization-requests"        , "routing_key": "binarization"         , "durable": True},
+    {"name": "resize-requests"              , "routing_key": "resize"               , "durable": True},
+    {"name": "count-people-requests"        , "routing_key": "count_people"         , "durable": True},
+    {"name": "object-detection-requests"    , "routing_key": "object_detection"     , "durable": True},
+    {"name": "background-removal-requests"  , "routing_key": "background_removal"   , "durable": True},
+    {"name": "watermark-requests"           , "routing_key": "requests.watermark"   , "durable": True},
+    {"name": "results"                      , "routing_key": "results"              , "durable": True},
 ]
