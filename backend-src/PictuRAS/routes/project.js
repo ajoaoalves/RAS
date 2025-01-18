658e7fb22a5bfcba77d068378848b756d3a45521
Marta <martinhagoncalves18@gmail.com>
2025-01-18 01:18:20 +0000

    route de execução do projeto

--- backend-src/PictuRAS/routes/project.js
+++ backend-src/PictuRAS/routes/project.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Project = require("../controllers/project")
+const sendToQueue = require('../controllers/send_to_queue');
 
 /* GET home page. */
 router.get('/projects', function(req, res, next) {
@@ -20,6 +21,27 @@ router.post('/projects', function(req, res) {
   Project.insert(req.body)
   .then(data => res.status(201).jsonp(data))
   .catch(erro => res.status(523).jsonp(erro))
+});
+
+router.put('/projects/:id/exec', function (req, res) {
+  console.log(req.body);
+
+  Project.updateProject(req.params.id, req.body)
+    .then(async (data) => {
+      // Enviar o projeto atualizado para a fila
+      try {
+        await sendToQueue(req.body); // `data` contém o projeto atualizado
+        console.log('Projeto enviado para a fila com sucesso.');
+        res.status(201).jsonp(data);
+      } catch (queueError) {
+        console.error('Erro ao enviar projeto para a fila:', queueError);
+        res.status(201).jsonp({
+          message: 'Projeto atualizado, mas não foi possível enviar para a fila.',
+          project: data,
+        });
+      }
+    })
+    .catch((erro) => res.status(524).jsonp(erro));
 });
 
 router.put('/projects/:id', function(req, res) {
