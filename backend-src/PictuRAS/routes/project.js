25b3699dde5f55156a1b2600dc80eab31546d161
Marta <martinhagoncalves18@gmail.com>
2025-01-18 15:22:16 +0000

    Backend sem execução de projetos a funcionar

--- backend-src/PictuRAS/routes/project.js
+++ backend-src/PictuRAS/routes/project.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var Project = require("../controllers/project")
-const sendToQueue = require('../controllers/send_to_queue');
 
 /* GET home page. */
 router.get('/projects', function(req, res, next) {
@@ -23,26 +22,7 @@ router.post('/projects', function(req, res) {
   .catch(erro => res.status(523).jsonp(erro))
 });
 
-router.put('/projects/:id/exec', function (req, res) {
-  console.log(req.body);
 
-  Project.updateProject(req.params.id, req.body)
-    .then(async (data) => {
-      // Enviar o projeto atualizado para a fila
-      try {
-        await sendToQueue(req.body); // `data` contém o projeto atualizado
-        console.log('Projeto enviado para a fila com sucesso.');
-        res.status(201).jsonp(data);
-      } catch (queueError) {
-        console.error('Erro ao enviar projeto para a fila:', queueError);
-        res.status(201).jsonp({
-          message: 'Projeto atualizado, mas não foi possível enviar para a fila.',
-          project: data,
-        });
-      }
-    })
-    .catch((erro) => res.status(524).jsonp(erro));
-});
 
 router.put('/projects/:id', function(req, res) {
   console.log(req.body)
