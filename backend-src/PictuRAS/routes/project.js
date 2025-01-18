6e2bdf4138fac3421cb17e4d6b5302a9d3d87b9a
Gustominox <agstcmps.111@gmail.com>
2025-01-18 15:49:40 +0000

    change url to have users

--- backend-src/PictuRAS/routes/project.js
+++ backend-src/PictuRAS/routes/project.js
@@ -3,39 +3,39 @@ var router = express.Router();
 var Project = require("../controllers/project")
 
 /* GET home page. */
-router.get('/projects', function(req, res, next) {
+router.get('/projects', function (req, res, next) {
   Project.list()
-  .then(data => res.jsonp(data))
-  .catch(erro => res.jsonp(erro))
+    .then(data => res.jsonp(data))
+    .catch(erro => res.jsonp(erro))
 });
 
-router.get('/projects/:id', function(req, res) {
+router.get('/users/:userId/projects/:id', function (req, res) {
   Project.findById(req.params.id)
-  .then(data => res.jsonp(data))
-  .catch(erro => res.jsonp(erro))
+    .then(data => res.jsonp(data))
+    .catch(erro => res.jsonp(erro))
 });
 
-router.post('/projects', function(req, res) {
+router.post('/users/:userId/projects', function (req, res) {
   console.log(req.body)
   Project.insert(req.body)
-  .then(data => res.status(201).jsonp(data))
-  .catch(erro => res.status(523).jsonp(erro))
+    .then(data => res.status(201).jsonp(data))
+    .catch(erro => res.status(523).jsonp(erro))
 });
 
 
 
-router.put('/projects/:id', function(req, res) {
+router.put('/users/:userId/projects/:id', function (req, res) {
   console.log(req.body)
   Project.updateProject(req.params.id, req.body)
-  .then(data => res.status(201).jsonp(data))
-  .catch(erro => res.status(524).jsonp(erro))
+    .then(data => res.status(201).jsonp(data))
+    .catch(erro => res.status(524).jsonp(erro))
 });
 
-router.delete('/projects/:id', function(req, res) {
+router.delete('/users/:userId/projects/:id', function (req, res) {
   console.log(req.body)
   Project.deleteProject(req.params.id, req.body)
-  .then(data => res.status(200).jsonp(data))
-  .catch(erro => res.status(525).jsonp(erro))
+    .then(data => res.status(200).jsonp(data))
+    .catch(erro => res.status(525).jsonp(erro))
 });
 
 module.exports = router;
